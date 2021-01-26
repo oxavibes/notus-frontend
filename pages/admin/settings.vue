e0c0e2bc6383b31e79af680305525f690bccb680
Stefano <katteos@hotmail.com>
2021-01-26 19:12:47 -0400

    Add Settings page

---
+++ pages/admin/settings.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex flex-wrap">
+    <div class="w-full px-4">
+      <CardSettings />
+    </div>
+  </div>
+</template>
+<script>
+import CardSettings from '@/components/cards/CardSettings.vue'
+
+export default {
+  components: {
+    CardSettings,
+  },
+  layout: 'Admin',
+}
+</script>
