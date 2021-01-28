38c6d930d9e1b275633bdfd837cd19914f081b1b
Stefano <katteos@hotmail.com>
2021-01-27 23:13:03 -0400

    Update admin layout

--- layouts/Admin.vue
+++ layouts/Admin.vue
@@ -1,26 +1,22 @@
 <template>
   <div>
-    <sidebar />
-    <div class="relative md:ml-64">
+    <!-- <sidebar /> -->
+    <div class="relative h-screen">
       <admin-navbar />
-      <!-- <header-stats /> -->
-      <div class="px-4 md:px-10 mx-auto w-full pt-8 md:pt-24">
+      <div class="px-4 pt-24 md:px-10 mx-auto w-full md:pt-8 md:pt-24">
         <Nuxt />
-        <footer-admin />
       </div>
     </div>
   </div>
 </template>
 <script>
-import AdminNavbar from '~/components/navbars/AdminNavbar.vue'
-import Sidebar from '~/components/sidebar/Sidebar.vue'
-import FooterAdmin from '~/components/footers/FooterAdmin.vue'
+import AdminNavbar from '@/components/navbars/AdminNavbar.vue'
+// import Sidebar from '@/components/sidebar/Sidebar.vue'
 
 export default {
   components: {
     AdminNavbar,
-    Sidebar,
-    FooterAdmin,
+    // Sidebar,
   },
 }
 </script>
