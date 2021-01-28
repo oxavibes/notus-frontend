513680c67baffc4a61900a1fd48c6dbd66211703
Stefano <katteos@hotmail.com>
2021-01-27 23:16:06 -0400

    Update AuthNavbar

--- components/navbars/AuthNavbar.vue
+++ components/navbars/AuthNavbar.vue
@@ -8,12 +8,12 @@
       <div
         class="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start"
       >
-        <router-link
+        <NuxtLink
           class="text-white text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase"
           to="/"
         >
           Notus
-        </router-link>
+        </NuxtLink>
       </div>
     </div>
   </nav>
